e11871c816de364ebc445f25e83d74dcfb9acb5a
Enno Hyttrek <ehy.global@gmail.com>
2022-07-06 11:08:04 +0200

    Version 1.2
    Added wrappers

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <?php get_header(); ?>
 <header>
+    <div class="wrapper header-wrapper">
     <h1><?php bloginfo('title'); ?></h1>
+        </div>
 </header>
 
-            <?php wp_nav_menu( array( 
+<?php wp_nav_menu( array( 
         'theme_location' => 'main-menu',
         'container' => 'nav',
         'container_class'   => "main-nav",
         'container_id'      => "main-nav" ) ); ?>
-
-<main>
-<?php 
+<div>
+    <main class="wrapper main-wrapper">
+        <?php 
 if ( have_posts() ) : 
     while ( have_posts() ) : the_post(); ?>
-    
-    <article>
-       <h2 class="my-post-title"><?php the_title(); ?></h2>
-    <figure><?php echo the_post_thumbnail( 'thumbnail' )?></figure>
-        <?php the_content(); ?>
+
+        <article>
+            <h2 class="my-post-title"><?php the_title(); ?></h2>
+            <figure><?php echo the_post_thumbnail( 'thumbnail' )?></figure>
+            <?php the_content(); ?>
         </article>
-    <?php
+        <?php
     endwhile; 
 endif; 
 ?>
-</main>
+    </main>
+</div>
 <?php get_footer(); ?>
